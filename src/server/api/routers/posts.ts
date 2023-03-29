1bd721e2b327bcdea6e78d849e1a42e6bdcaad2c
Zak <Zaksmith04@gmail.com>
2023-03-29 17:00:18 -0400

    feat: user auth, post validation

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -5,7 +5,7 @@ import { clerkClient } from "@clerk/nextjs/server";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { createTRPCRouter, publicProcedure,privateProcedure } from "../trpc";
 
 
 const filterUserForClient = (user: User) =>{
@@ -44,4 +44,21 @@ export const postsRouter = createTRPCRouter({
     };
     });
   }),
+
+  create: privateProcedure
+  .input(
+    z.object({
+    content:z.string().emoji().min(1).max(280)
+  })
+  )
+  .mutation(async ({ ctx,input }) => {
+  const authorId = ctx.currentUser.id;
+  const post = await ctx.prisma.post.create({
+    data:{
+      authorId,
+      content: input.content,
+    },
+  });
+return post;
+}),
 });
