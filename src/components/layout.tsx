1b57a38361779778a933c34293fc776f0b2edbef
Zak <Zaksmith04@gmail.com>
2023-03-31 08:40:36 -0400

    fix: adjust layout responsiveness

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,11 +3,13 @@ import type { PropsWithChildren } from "react";
 export const PageLayout = (props: PropsWithChildren) => {
   return (
     <main className="flex h-screen justify-center ">
-      <div className="w-full md:max-w-2xl">Nav Menu</div>
-      <div className="h-full w-full overflow-y-scroll border-x border-slate-400 md:max-w-2xl">
+      <div className="hidden w-full md:flex md:max-w-2xl">Nav Menu</div>
+      <div className="h-full w-full min-w-full overflow-y-scroll border-x border-slate-400 md:min-w-[550px] md:max-w-2xl">
         {props.children}
       </div>
-      <div className="w-full md:max-w-2xl">Related Posts/Tags, Search </div>
+      <div className="hidden w-full md:flex md:max-w-2xl">
+        Related Posts/Tags, Search{" "}
+      </div>
     </main>
   );
 };
