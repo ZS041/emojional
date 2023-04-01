ac1263e88ff14f4b167924cf54685ead83a6c4f6
Zak <Zaksmith04@gmail.com>
2023-04-01 00:33:43 -0400

    feat: reply modal

--- src/components/postview.tsx
+++ src/components/postview.tsx
@@ -13,18 +13,79 @@ import chatBubble from "~/images/UI/chatbubble.svg";
 import { useUser } from "@clerk/nextjs";
 
 import { LoadingSpinner } from "~/components/loading";
-import { useState } from "react";
 
 dayjs.extend(relativeTime);
 
 import toast from "react-hot-toast";
 
-type PostWithUser = RouterOutputs["posts"]["getAll"][number];
-type CreateReplyWizardProps = {
+import { Fragment, useState } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+
+type ReplyModalProps = {
   postId: string;
+
+  toggleReplyModal: () => void;
+};
+const ReplyModal = ({ postId, toggleReplyModal }: ReplyModalProps) => {
+  const [open, setOpen] = useState(true);
+
+  return (
+    <Transition.Root show={open} as={Fragment}>
+      <Dialog as="div" className="relative z-10" onClose={toggleReplyModal}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 z-10 overflow-y-auto">
+          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-slate-800 px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
+                <div>
+                  <div className=" text-center ">
+                    <div className="mt-2">
+                      <CreateReplyWizard
+                        postId={postId}
+                        toggleReplyModal={toggleReplyModal}
+                      />
+                    </div>
+                  </div>
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition.Root>
+  );
 };
 
-const CreateReplyWizard = ({ postId }: CreateReplyWizardProps) => {
+type PostWithUser = RouterOutputs["posts"]["getAll"][number];
+
+type CreateReplyWizardProps = {
+  postId: string;
+  toggleReplyModal: () => void; // Add toggleReplyModal as a prop
+};
+
+const CreateReplyWizard = ({
+  postId,
+  toggleReplyModal,
+}: CreateReplyWizardProps) => {
   const { user } = useUser();
 
   const ctx = api.useContext();
@@ -33,6 +94,7 @@ const CreateReplyWizard = ({ postId }: CreateReplyWizardProps) => {
     onSuccess: () => {
       setInput("");
       void ctx.replies.getRepliesByPostId.invalidate();
+      toggleReplyModal(); // call toggleReplyModal function to close the modal
     },
     onError: (e) => {
       const errorMessage = e.data?.zodError?.fieldErrors.content;
@@ -53,7 +115,7 @@ const CreateReplyWizard = ({ postId }: CreateReplyWizardProps) => {
   return (
     <div className="flex w-full gap-3">
       <input
-        placeholder="Type some emojis!"
+        placeholder="Reply with an emoji!"
         className="grow bg-transparent outline-none"
         value={input}
         type="text"
@@ -88,6 +150,11 @@ const CreateReplyWizard = ({ postId }: CreateReplyWizardProps) => {
 
 export const PostView = (props: PostWithUser) => {
   const { post, author } = props;
+  const [showReplyModal, setShowReplyModal] = useState(false);
+
+  const toggleReplyModal = () => {
+    setShowReplyModal(!showReplyModal);
+  };
   return (
     <div key={post.id} className=" border-b border-slate-400 p-4">
       <div className="flex gap-3">
@@ -114,9 +181,12 @@ export const PostView = (props: PostWithUser) => {
           </div>
         </div>
       </div>
-      <div className="flex h-3 justify-between px-[72px] align-bottom">
-        <CreateReplyWizard postId={post.id} />
-        <button>
+      <div className="flex h-5 justify-between px-[72px] align-bottom">
+        {showReplyModal && (
+          <ReplyModal postId={post.id} toggleReplyModal={toggleReplyModal} />
+        )}
+
+        <button onClick={toggleReplyModal}>
           <Image src={chatBubble} alt="Reply" height={18} width={18} />
         </button>
       </div>
