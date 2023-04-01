f724af60aef11be0245b81e3c0483ab13f296ac2
Zak <Zaksmith04@gmail.com>
2023-04-01 08:46:41 -0400

    fix: styling reply modal

--- src/components/postview.tsx
+++ src/components/postview.tsx
@@ -56,10 +56,10 @@ const ReplyModal = ({ postId, toggleReplyModal }: ReplyModalProps) => {
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-slate-800 px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
+              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-slate-800 px-4 pb-4  text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
                 <div>
                   <div className=" text-center ">
-                    <div className="mt-2">
+                    <div className="">
                       <CreateReplyWizard
                         postId={postId}
                         toggleReplyModal={toggleReplyModal}
